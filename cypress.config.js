2fe9e82ecbbc80f8f0615dafc1b2343ac96d3cfe
Bartosz Brozyna <bartosz.brozyna@stxnext.pl>
2023-08-20 16:35:13 +0200

    added reporter scripts and packages

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+    "reporter": "cypress-multi-reporters",
+    "reporterOptions": {
+        "configFile": "reporter-config.json"
+    },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
